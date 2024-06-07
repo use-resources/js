a9a2ff50cf124ead642321cf7ae8e0ae7a30b94c
victor <55814035+m-vnio@users.noreply.github.com>
2024-06-07 17:45:44 -0500

    Update doodstream-media.js

--- doodstream-media.js
+++ doodstream-media.js
@@ -1,1 +1,16 @@
 
+class DoodstreamMedia {
+  get(doodstream) {
+    fetch(
+      `https://l8qn2l7t-3500.brs.devtunnels.ms/doodstream?url=${doodstream}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        fetch("https://d000d.com" + json.url)
+          .then((res) => res.text())
+          .then((text) => {
+            console.log(text + json.makePlay);
+          });
+      });
+  }
+}
