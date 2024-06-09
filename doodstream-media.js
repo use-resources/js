d3d0bcb89c330d4dde82b090b3457348a07d858b
victor <55814035+m-vnio@users.noreply.github.com>
2024-06-09 14:08:45 -0500

    Update doodstream-media.js

--- doodstream-media.js
+++ doodstream-media.js
@@ -1,15 +1,14 @@
-
 class DoodstreamMedia {
   async get(doodstream) {
     return await fetch(
       `https://l8qn2l7t-3500.brs.devtunnels.ms/doodstream?url=${doodstream}`
     )
       .then((res) => res.json())
-      .then((json) => {
-        fetch("https://d000d.com" + json.url)
+      .then(async (json) => {
+        return await fetch("https://d000d.com" + json.url)
           .then((res) => res.text())
           .then((text) => {
-            return text + json.makePlay
+            return text + json.makePlay;
           });
       });
   }
