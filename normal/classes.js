3be68f76ad5a39f63c8ac2ee0f8bc32a19a54571
victor <55814035+m-vnio@users.noreply.github.com>
2024-10-25 16:50:07 -0500

    Update classes.js

--- normal/classes.js
+++ normal/classes.js
@@ -205,3 +205,137 @@ class Cookie {
     document.cookie = `${name}=; expires=${expires}; path=${path}; domain=${domain};`;
   }
 }
+
+class ElementMakeMovable {
+  constructor(element) {
+    this._element = element;
+    this._events = {};
+  }
+
+  on = (type, callback) => {
+    this._events[type] = callback;
+  };
+
+  start = () => {
+    const start = (element) => {
+      var draggable = element;
+      var initialX, initialY, currentX, currentY;
+
+      const startDragging = (e) => {
+        if (e.type === "touchstart") {
+          initialX = e.touches[0].clientX - draggable.offsetLeft;
+          initialY = e.touches[0].clientY - draggable.offsetTop;
+        } else {
+          initialX = e.clientX - draggable.offsetLeft;
+          initialY = e.clientY - draggable.offsetTop;
+        }
+
+        if (typeof this._events.start == "function") {
+          this._events.start({
+            e,
+            xy: {
+              initial: {
+                x: initialX,
+                y: initialY,
+              },
+              current: {
+                x: currentX,
+                y: currentY,
+              },
+            },
+            target: draggable,
+          });
+        }
+
+        if (e.type === "mousedown") {
+          e.preventDefault();
+        }
+
+        element.addEventListener("touchmove", drag, { passive: false });
+        element.addEventListener("touchend", stopDragging);
+        element.addEventListener("mousemove", drag);
+        element.addEventListener("mouseup", stopDragging);
+        element.addEventListener("mouseleave", stopDragging);
+      };
+
+      const drag = (e) => {
+        e.preventDefault();
+
+        if (e.type === "touchmove") {
+          currentX = e.touches[0].clientX - initialX;
+          currentY = e.touches[0].clientY - initialY;
+        } else {
+          currentX = e.clientX - initialX;
+          currentY = e.clientY - initialY;
+        }
+
+        if (typeof this._events.move == "function") {
+          this._events.move({
+            e,
+            xy: {
+              initial: {
+                x: initialX,
+                y: initialY,
+              },
+              current: {
+                x: currentX,
+                y: currentY,
+              },
+            },
+            target: draggable,
+          });
+        }
+      };
+
+      const stopDragging = (e) => {
+        if (typeof this._events.end == "function") {
+          this._events.end({
+            e,
+            xy: {
+              initial: {
+                x: initialX,
+                y: initialY,
+              },
+              current: {
+                x: currentX,
+                y: currentY,
+              },
+            },
+            target: draggable,
+          });
+        }
+
+        if (e.touches && e.touches.length > 0) return;
+        element.removeEventListener("touchmove", drag, { passive: false });
+        element.removeEventListener("touchend", stopDragging);
+        element.removeEventListener("mousemove", drag);
+        element.removeEventListener("mouseup", stopDragging);
+        element.removeEventListener("mouseleave", stopDragging);
+      };
+
+      draggable.addEventListener("touchstart", startDragging, {
+        passive: false,
+      });
+      draggable.addEventListener("mousedown", startDragging);
+    };
+
+    start(this._element);
+  };
+}
+
+class EncodeTemplateString {
+  static {
+    this._input = document.createElement("input");
+    this._textarea = document.createElement("textarea");
+  }
+
+  static toInput = (string = "") => {
+    this._input.setAttribute("value", string);
+    return this._input.outerHTML.slice(14, -2);
+  };
+
+  static toTextarea = (string = "") => {
+    this._textarea.innerHTML = string;
+    return this._textarea.innerHTML;
+  };
+}
