d594cce95fcfba689eb5c86ec3f4061d2f757f17
victor <55814035+m-vnio@users.noreply.github.com>
2025-01-15 13:16:02 -0500

    Add files via upload

--- v1/api-web-cuevana.js
+++ v1/api-web-cuevana.js
@@ -1,63 +1,44 @@
 class ApiWebCuevana {
-  static _resolveJson(text, resolve) {
-    try {
-      const $text = document.createElement("html");
-      $text.innerHTML = text;
+  static _fetchWebResolve(url, resolve) {
+    fetch(url)
+      .then((res) => res.text())
+      .then((text) => {
+        try {
+          const $text = document.createElement("html");
+          $text.innerHTML = text;
 
-      Array.from($text.querySelectorAll("img")).forEach((img) => {
-        img.setAttribute("srcset", "");
-        img.setAttribute("src", "");
-      });
+          Array.from($text.querySelectorAll("img")).forEach((img) => {
+            img.setAttribute("srcset", "");
+            img.setAttribute("src", "");
+          });
 
-      const $data = $text.querySelector("#__NEXT_DATA__");
-      const dataJson = JSON.parse($data?.textContent);
+          const $data = $text.querySelector("#__NEXT_DATA__");
+          const dataJson = JSON.parse($data?.textContent);
 
-      resolve(dataJson);
-    } catch (error) {
-      resolve({});
-    }
+          resolve(dataJson);
+        } catch (error) {
+          resolve(null);
+        }
+      })
+      .catch(() => resolve(null));
   }
 
   static pelicula(page = 1, genre = "") {
     return new Promise((resolve) => {
       page = Math.max(1, page);
 
-      fetch(
-        !genre
-          ? `https://cuevana.biz/peliculas/page/${page}`
-          : `https://cuevana.biz/genero/${genre}/page/${page}`
-      )
-        .then((res) => res.text())
-        .then((text) => {
-          try {
-            const $text = document.createElement("html");
-            $text.innerHTML = text;
+      const url = !genre
+        ? `https://cuevana.biz/peliculas/page/${page}`
+        : `https://cuevana.biz/genero/${genre}/page/${page}`;
 
-            Array.from($text.querySelectorAll("img")).forEach((img) => {
-              img.setAttribute("srcset", "");
-              img.setAttribute("src", "");
-            });
-
-            const $data = $text.querySelector("#__NEXT_DATA__");
-            const dataJson = JSON.parse($data?.textContent);
-
-            resolve(dataJson);
-          } catch (error) {
-            resolve({});
-          }
-        })
-        .then(() => resolve({}));
+      this._fetchWebResolve(url, resolve);
     });
   }
 
   static peliculaId(id) {
     return new Promise((resolve) => {
-      fetch(`https://cuevana.biz/pelicula/${id}/${id}`)
-        .then((res) => res.text())
-        .then((text) => {
-          this._resolveJson(text, resolve);
-        })
-        .then(() => resolve({}));
+      const url = `https://cuevana.biz/pelicula/${id}/${id}`;
+      this._fetchWebResolve(url, resolve);
     });
   }
 
@@ -65,12 +46,8 @@ class ApiWebCuevana {
     return new Promise((resolve) => {
       page = Math.max(1, page);
 
-      fetch(`https://cuevana.biz/series/page/${page}`)
-        .then((res) => res.text())
-        .then((text) => {
-          this._resolveJson(text, resolve);
-        })
-        .then(() => resolve({}));
+      const url = `https://cuevana.biz/series/page/${page}`;
+      this._fetchWebResolve(url, resolve);
     });
   }
 
@@ -80,23 +57,14 @@ class ApiWebCuevana {
       url +=
         season && chapter ? `/temporada/${season}/episodio/${chapter}` : "";
 
-      fetch(url)
-        .then((res) => res.text())
-        .then((text) => {
-          this._resolveJson(text, resolve);
-        })
-        .then(() => resolve({}));
+      this._fetchWebResolve(url, resolve);
     });
   }
 
   static search(search = "") {
     return new Promise((resolve) => {
-      fetch(`https://cuevana.biz/search?q=${encodeURIComponent(search)}`)
-        .then((res) => res.text())
-        .then((text) => {
-          this._resolveJson(text, resolve);
-        })
-        .then(() => resolve({}));
+      let url = `https://cuevana.biz/search?q=${encodeURIComponent(search)}`;
+      this._fetchWebResolve(url, resolve);
     });
   }
 
@@ -132,7 +100,8 @@ class ApiWebCuevana {
             } catch (error) {
               resolve(null);
             }
-          });
+          })
+          .catch(() => resolve(null));
       } catch (error) {
         resolve(null);
       }
