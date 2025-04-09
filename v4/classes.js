ada6b4d16404480eb2bdd1344eb3e6ce2d8c0fb7
victor <55814035+m-vnio@users.noreply.github.com>
2025-04-09 00:45:00 -0500

    Add files via upload

---
+++ v4/classes.js
@@ -0,0 +1,453 @@
+window.myResourceClass = (function (_) {
+  _.MyArray = class {
+    static range(start, end) {
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      }
+
+      const result = [];
+      for (let i = start; i <= end; i++) {
+        result.push(i);
+      }
+
+      return result;
+    }
+    static search(arr, predicate) {
+      if (typeof predicate == "function") {
+        for (let i = 0; i < arr.length; i++) {
+          const predicateResult = predicate(arr[i], i, arr);
+          if (predicateResult) {
+            return [arr[i], i, predicateResult];
+          }
+        }
+      }
+
+      return [null, -1, null];
+    }
+    static copy(arr) {
+      return [...arr];
+    }
+    static shuffle(arr) {
+      const copy = [...arr];
+      const array = [];
+
+      while (copy.length) {
+        const rand = Math.floor(Math.random() * copy.length);
+        array.push(...copy.splice(rand, 1));
+      }
+
+      return array;
+    }
+    static array(array = []) {
+      if (
+        Array.isArray(array) ||
+        (array && typeof array[Symbol.iterator] === "function")
+      ) {
+        return Array.from(array);
+      }
+      return [];
+    }
+    static changeIndex(arr, index1, index2) {
+      if (
+        index1 >= 0 &&
+        index1 < arr.length &&
+        index2 >= 0 &&
+        index2 < arr.length
+      ) {
+        const temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+      }
+    }
+    static split(string, initialArray = []) {
+      const array = Array.from(initialArray);
+
+      const reduce = array.reduce((prev, curr) => {
+        string.replaceAll(curr, (match, index, indexMatch, stringMatch) => {
+          const key = stringMatch ? indexMatch : index;
+          const value = stringMatch
+            ? indexMatch + index.length
+            : index + match.length;
+
+          prev[key] = value;
+
+          return "";
+        });
+
+        return prev;
+      }, {});
+
+      return Array.from(string).reduce(
+        (prev, curr, index, arr) => {
+          prev.some = Object.entries(reduce).some(([key, value]) => {
+            return parseInt(key) <= index && index < value;
+          });
+
+          if (!prev.some) {
+            prev.string += curr;
+            prev.add = true;
+          }
+
+          if ((prev.add && prev.some) || !arr[index + 1]) {
+            prev.array.push(prev.string);
+            prev.string = "";
+            prev.add = false;
+          }
+
+          return prev;
+        },
+        {
+          string: "",
+          array: [],
+          some: false,
+          add: false,
+        }
+      ).array;
+    }
+    static map(arr, callback, includeFalsy = true) {
+      const result = [];
+
+      for (let i = 0; i < arr.length; i++) {
+        const value = callback(arr[i], i, arr);
+        if (includeFalsy || value) {
+          result.push(value);
+        }
+      }
+
+      return result;
+    }
+  };
+  _.MyColor = class {
+    static toRgb(object = {}) {
+      if (object?.hex) {
+        let hex = object.hex;
+        hex = hex.replace(/^#/, "");
+        hex = hex.slice(0, 6);
+
+        if (hex.length === 3) {
+          hex = hex
+            .split("")
+            .map((char) => char + char)
+            .join("");
+        }
+
+        const bigint = parseInt(hex, 16);
+        const r = (bigint >> 16) & 255;
+        const g = (bigint >> 8) & 255;
+        const b = bigint & 255;
+
+        return {
+          string: `rgb(${r}, ${g}, ${b})`,
+          array: [r, g, b],
+        };
+      }
+
+      if (object?.color) {
+        const hex = this.toHex({ color: object.color });
+        return this.toRgb({ hex });
+      }
+
+      return {
+        string: "",
+        rgb: [],
+      };
+    }
+    static toHex(object = {}) {
+      if (object?.rgb) {
+        const [r = 0, g = 0, b = 0] = Array.isArray(object.rgb)
+          ? object.rgb
+          : [];
+
+        return `#${((1 << 24) | (r << 16) | (g << 8) | b)
+          .toString(16)
+          .slice(1)
+          .toUpperCase()}`;
+      }
+
+      if (object?.color) {
+        const ctx = document.createElement("canvas").getContext("2d");
+        ctx.fillStyle = object.color;
+        return ctx.fillStyle.toUpperCase();
+      }
+
+      return "";
+    }
+    static toDark(object = {}, amount = 20) {
+      if (object?.hex) {
+        const rgb = this.toRgb({ hex: object.hex });
+        return this.toDark({ rgb: rgb.array }, amount);
+      }
+
+      if (object?.color) {
+        const rgb = this.toRgb({ color: object.color });
+        return this.toDark({ rgb: rgb.array }, amount);
+      }
+
+      if (object?.rgb) {
+        let [r = 0, g = 0, b = 0] = Array.isArray(object.rgb) ? object.rgb : [];
+
+        r = Math.max(0, r - amount);
+        g = Math.max(0, g - amount);
+        b = Math.max(0, b - amount);
+
+        return this.toHex({ rgb: [r, g, b] });
+      }
+
+      return "";
+    }
+  };
+  _.MyImage = class {
+    static async canvas(url) {
+      return new Promise((resolve, reject) => {
+        const image = new Image();
+
+        image.onload = () => {
+          const canvas = document.createElement("canvas");
+          const ctx = canvas.getContext("2d");
+
+          canvas.width = image.width;
+          canvas.height = image.height;
+
+          ctx.drawImage(image, 0, 0);
+          resolve({
+            canvas,
+            ctx,
+            url,
+          });
+        };
+
+        image.onerror = reject;
+
+        image.crossOrigin = "Anonymous";
+        image.src = url;
+      });
+    }
+    static async resize(url, resizes) {
+      return new Promise((resolve, reject) => {
+        if (!Array.isArray(resizes)) {
+          return reject({ status: false, images: [] });
+        }
+
+        const image = new Image();
+
+        image.addEventListener("load", () => {
+          const images = resizes.map((resize) => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+            const width = resize.both ?? resize.width;
+            const height = resize.both ?? resize.height;
+            canvas.width = width;
+            canvas.height = height;
+            const imgAspectRatio = image.width / image.height;
+            const canvasAspectRatio = width / height;
+            let drawWidth, drawHeight, offsetX, offsetY;
+            if (imgAspectRatio > canvasAspectRatio) {
+              drawWidth = image.height * canvasAspectRatio;
+              drawHeight = image.height;
+              offsetX = (image.width - drawWidth) / 2;
+              offsetY = 0;
+            } else {
+              drawWidth = image.width;
+              drawHeight = image.width / canvasAspectRatio;
+              offsetX = 0;
+              offsetY = (image.height - drawHeight) / 2;
+            }
+            ctx.drawImage(
+              image,
+              offsetX,
+              offsetY,
+              drawWidth,
+              drawHeight,
+              0,
+              0,
+              width,
+              height
+            );
+            return {
+              width: width,
+              height: height,
+              canvas,
+            };
+          });
+          resolve({ status: true, images });
+        });
+
+        image.addEventListener("error", () => {
+          reject({ status: false, images: [] });
+        });
+
+        image.crossOrigin = "Anonymous";
+        image.src = url;
+      });
+    }
+  };
+  _.Trigger = class {
+    constructor() {
+      this._map = new Map();
+    }
+
+    on(type, callback) {
+      if (typeof type != "string") {
+        return null;
+      }
+
+      const map = this._map;
+      const symbol = Symbol();
+
+      const off = () => {
+        return map.delete(symbol);
+      };
+
+      map.set(symbol, {
+        type,
+        symbol,
+        callback: (value) => {
+          callback(value, off);
+        },
+      });
+
+      return off;
+    }
+
+    off(type) {
+      const map = this._map;
+
+      if (typeof type != "string") {
+        map.clear();
+        return;
+      }
+
+      Array.from(map.keys()).forEach((key) => {
+        const object = map.get(key);
+
+        if (object.type === type) {
+          map.delete(key);
+        }
+      });
+    }
+
+    dispatch(type, value) {
+      if (typeof type != "string") {
+        return false;
+      }
+
+      const map = this._map;
+
+      map.forEach((object) => {
+        if (object.type === type) {
+          object.callback(value);
+        }
+      });
+
+      return true;
+    }
+  };
+  _.Trim = class {
+    static left = (text = "", symbol = "") => {
+      if (text != "" && symbol != "") {
+        while (text.startsWith(symbol)) text = text.slice(1);
+      }
+      return text;
+    };
+    static right = (text = "", symbol = "") => {
+      if (text != "" && symbol != "") {
+        while (text.endsWith(symbol)) text = text.slice(0, -1);
+      }
+      return text;
+    };
+    static both = (text = "", symbol = "") => {
+      if (text != "" && symbol != "") {
+        while (text.startsWith(symbol)) text = text.slice(1);
+        while (text.endsWith(symbol)) text = text.slice(0, -1);
+      }
+      return text;
+    };
+  };
+  _.MatchPath = class {
+    constructor() {
+      this._params = {};
+      this._set = [];
+    }
+
+    _path = (string) => {
+      if (!string) {
+        return [""];
+      }
+
+      const array = string.split("/").filter(Boolean);
+
+      if (!array.length) {
+        return [""];
+      }
+
+      return array;
+    };
+
+    get = (match = "") => {
+      this._params = {};
+
+      const path = this._path(match);
+      const exists = this._set.find((set) => {
+        if (set.all != -1) {
+          const stringFirst = path.join("/").concat("/");
+          const stringSecond = set.path.arrayString
+            .slice(0, set.all)
+            .join("/")
+            .concat("/");
+
+          return stringFirst.startsWith(stringSecond) || stringSecond == "/";
+        }
+
+        if (set.path.arrayObject.length == path.length) {
+          const valid = path.every((name, i) => {
+            if (set.path.arrayObject[i].status) {
+              this._params[set.path.arrayObject[i].name] = name;
+              return true;
+            }
+
+            return name === set.path.arrayObject[i].name;
+          });
+
+          return valid;
+        }
+
+        return false;
+      });
+
+      return {
+        status: Boolean(exists),
+        data: exists?.data ?? null,
+        params: this.params(),
+      };
+    };
+
+    set = (key, array = []) => {
+      this._set = array.map((object) => {
+        const array = this._path(object[key]);
+        let all = -1;
+
+        return {
+          data: object,
+          path: {
+            arrayObject: array.map((string, index) => {
+              if (all == -1 && string == "*") all = index;
+
+              return {
+                status: string.startsWith(":"),
+                name: string.startsWith(":") ? string.slice(1) : string,
+              };
+            }),
+            arrayString: array,
+          },
+          all,
+        };
+      });
+    };
+
+    params = () => {
+      return { ...this._params };
+    };
+  };
+  return _;
+})({});
---
+++ v4/functions.js
@@ -0,0 +1,311 @@
+window.myResourceFunction = (function (_) {
+  _.rand = function (...params) {
+    const [max, min = 0] = params.reverse();
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+  };
+  _.createElement = function (html, callback) {
+    const template = document.createElement("div");
+    template.innerHTML = html;
+    const element = template.children[0] || "";
+    template.innerHTML = "";
+
+    if (typeof callback == "function") {
+      callback(element);
+    }
+
+    return element;
+  };
+  _.createFragment = function (html, callback) {
+    const template = document.createElement("div");
+    const fragment = document.createDocumentFragment();
+
+    template.innerHTML = html;
+    fragment.append(...template.childNodes);
+
+    if (typeof callback == "function") {
+      callback(fragment);
+    }
+
+    return fragment;
+  };
+  _.objectElement = function (elements, attribute, remove = false) {
+    return Array.from(elements).reduce((prev, curr) => {
+      prev[curr.getAttribute(attribute)] = curr;
+      if (remove) curr.removeAttribute(attribute);
+      return prev;
+    }, {});
+  };
+  _.styleElement = function (array, styles) {
+    const elements = Array.isArray(array) ? array : [array];
+    const entries = Object.entries(styles ?? {});
+
+    elements.forEach((element) => {
+      if (element instanceof Element) {
+        entries.forEach(([key, value]) => {
+          element.style[key] = value;
+        });
+      }
+    });
+
+    return elements;
+  };
+  _.replaceChildren = function (element, children = {}) {
+    if (element instanceof Node) {
+      Object.entries(children ?? {}).forEach(([key, value]) => {
+        const query = element.querySelector(key);
+        if (query) {
+          query.replaceWith(value);
+          if (value instanceof Element) {
+            value.append(...query.childNodes);
+
+            Array.from(query.attributes).forEach((attribute) => {
+              value.setAttribute?.(
+                attribute.name,
+                `${value.getAttribute(attribute.name) || ""} ${
+                  attribute.value
+                }`.trim()
+              );
+            });
+          }
+        }
+      });
+    }
+
+    return element;
+  };
+  _.copyToClipboard = function (text = "") {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(text);
+    } else {
+      const textarea = document.createElement("textarea");
+      textarea.setAttribute("style", "position: fixed; top: 100%; left: 100%;");
+      textarea.value = text;
+      document.body.append(textarea);
+      textarea.select();
+      document.execCommand("copy");
+      textarea.remove();
+    }
+  };
+  _.countdown = function (number, object = {}) {
+    const condition = (type) => {
+      return Boolean(object instanceof Object ? object[type] : object);
+    };
+
+    const data = {
+      day: 0,
+      hour: 0,
+      minute: 0,
+      second: 0,
+    };
+
+    let seconds = parseInt(number);
+
+    if (condition("day")) {
+      data.day = Math.floor(seconds / (3600 * 24));
+      seconds %= 3600 * 24;
+    }
+
+    if (condition("hour")) {
+      data.hour = Math.floor(seconds / 3600);
+      seconds %= 3600;
+    }
+
+    if (condition("minute")) {
+      data.minute = Math.floor(seconds / 60);
+      seconds %= 60;
+    }
+
+    if (condition("second")) {
+      data.second = seconds;
+    }
+
+    return Object.freeze(data);
+  };
+  _.download = function (href = "", name = "") {
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = href;
+    a.download = name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+  _.getAspectRatio = function (currentDimensions, newDimensions) {
+    const { width: currentWidth, height: currentHeight } = currentDimensions;
+    const { width: newWidth, height: newHeight } = newDimensions;
+
+    if (newWidth) {
+      const scaleFactor = newWidth / currentWidth;
+      return { width: newWidth, height: currentHeight * scaleFactor };
+    } else if (newHeight) {
+      const scaleFactor = newHeight / currentHeight;
+      return { width: currentWidth * scaleFactor, height: newHeight };
+    } else {
+      return { width: currentWidth, height: currentHeight };
+    }
+  };
+  _.getUUID = function () {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        const r = (Math.random() * 16) | 0;
+        const v = c === "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  };
+  _.signal = function (initialValue) {
+    const object = new Object();
+    const map = new Map();
+    let value = initialValue;
+
+    Object.defineProperty(object, "value", {
+      get: () => {
+        return value;
+      },
+      set: (newValue) => {
+        if (newValue !== value) {
+          value = newValue;
+          map.forEach((callback) => callback());
+        }
+      },
+    });
+
+    Object.defineProperty(object, "on", {
+      value: (callback, immediate = true) => {
+        const symbol = Symbol();
+
+        const off = () => {
+          return map.delete(symbol);
+        };
+
+        map.set(symbol, () => {
+          callback(object.value, off);
+        });
+
+        if (Boolean(immediate)) {
+          callback(object.value, off);
+        }
+
+        return off;
+      },
+      writable: false,
+      enumerable: false,
+      configurable: false,
+    });
+
+    Object.defineProperty(object, "off", {
+      value: () => {
+        map.clear();
+        return map.size;
+      },
+      writable: false,
+      enumerable: false,
+      configurable: false,
+    });
+
+    Object.seal(object);
+    return object;
+  };
+  _.event = function (array, type, callback, options = {}) {
+    const elements = Array.isArray(array) ? array : [array];
+    const callbackNull = () => {};
+
+    return elements.map((element) => {
+      if (element && typeof element.addEventListener === "function") {
+        element.addEventListener(type, callback, options);
+        return () => {
+          element.removeEventListener(type, callback, options);
+        };
+      }
+
+      return callbackNull;
+    });
+  };
+  _.storage = function (storage, object, replace = true) {
+    return Object.entries(object).reduce((prev, curr) => {
+      const status = replace || !storage.getItem(curr[0]);
+
+      if (status) {
+        storage.setItem(
+          curr[0],
+          curr[1] instanceof Object ? JSON.stringify(curr[1]) : curr[1]
+        );
+      }
+
+      prev[curr[0]] = status;
+      return prev;
+    }, {});
+  };
+  _.percentage = function percentage(current, total) {
+    return (parseFloat(current) / parseFloat(total)) * 100;
+  };
+  _.encodeToParams = function (query) {
+    if (query instanceof Object) {
+      const encodeKeyValue = (key, value) => {
+        const encodeKey = encodeURIComponent(key);
+        const encodeValue = encodeURIComponent(String(value));
+
+        return `${encodeKey}=${encodeValue}`;
+      };
+
+      return Object.entries(query)
+        .map(([key, value]) => {
+          if (Array.isArray(value)) {
+            return value
+              .map((value) => {
+                return encodeKeyValue(key, value);
+              })
+              .join("&");
+          }
+          return encodeKeyValue(key, value);
+        })
+        .join("&");
+    }
+
+    return "";
+  };
+  _.encodeToInput = function (string = "") {
+    const input = document.createElement("input");
+    input.setAttribute("value", string);
+    return input.outerHTML.slice(14, -2);
+  };
+  _.encodeToTextarea = function (string = "") {
+    const textarea = document.createElement("textarea");
+    textarea.innerHTML = string;
+    return textarea.innerHTML;
+  };
+  _.callbackTryCatch = function (
+    callbackTry = null,
+    callbackCatch = null,
+    ...parameters
+  ) {
+    try {
+      if (typeof callbackTry == "function") return callbackTry(...parameters);
+      return callbackTry;
+    } catch (error) {
+      if (typeof callbackCatch == "function") return callbackCatch(error);
+      return callbackCatch;
+    }
+  };
+  _.callbackInterval = function callbackInterval(callback, time = 1000) {
+    let i = 0;
+    const interval = setInterval(() => {
+      if (callback(++i, interval)) {
+        clearInterval(interval);
+      }
+    }, parseInt(time) || 1000);
+    callback(i, interval);
+    return interval;
+  };
+  _.callbackTimeout = function (callback, initialTime = 0) {
+    let timeout = null;
+    const time = parseInt(initialTime) || 0;
+
+    return (...parameters) => {
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(callback, time, ...parameters);
+    };
+  };
+  return _;
+})({});
